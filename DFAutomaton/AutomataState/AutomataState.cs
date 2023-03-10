2df3d3bbeff22f4120becfb96e296a5c9e117ace
Andrei Gankin <gankin89@gmail.com>
2023-03-10 22:37:59 +0400

    Fixed bug with transitions not updated after backed next states collection filled during building.

--- DFAutomaton/AutomataState/AutomataState.cs
+++ DFAutomaton/AutomataState/AutomataState.cs
@@ -14,12 +14,11 @@ namespace DFAutomaton
             _nextStates = nextStates;
 
             Type = type;
-            Transitions = new HashSet<TTransition>(nextStates.Keys);
         }
 
         public StateType Type { get; }
 
-        public IReadOnlySet<TTransition> Transitions { get; }
+        public IReadOnlySet<TTransition> Transitions => new HashSet<TTransition>(_nextStates.Keys);
 
         public Option<AutomataNextState<TTransition, TState>> this[TTransition transition] =>
             _nextStates.GetValueOrNone(transition);
